d401e037daf365f7f09352d81fb5e96d2c250651
durch <durch@users.noreply.github.com>
2023-12-21 17:55:38 +0100

    Stub getting file contents

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from "fs";
+
 import { AdditionalContext, Payload, User } from "./interfaces";
 
 export function combinePayloads(
@@ -41,4 +43,42 @@ export function joinStrings(
 	second: string | undefined
 ): string {
 	return [first, second].filter((s) => s).join("\n");
+}
+
+export async function getImageContent(basePath: string, path: string) {
+	const contents = readFileSync(`${basePath}/${path}`);
+	const buffedInput = Buffer.from(contents).toString("base64");
+
+	// use the file extension to determine the mime type
+	// can we use a case statement here?
+	if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
+		return `data:image/jpeg;base64,${buffedInput}`;
+	} else if (path.endsWith(".gif")) {
+		return `data:image/gif;base64,${buffedInput}`;
+	}
+
+	// default to png
+	return `data:image/png;base64,${buffedInput}`;
+}
+
+export function isImage(path: string): boolean {
+	return (
+		path.endsWith(".png") ||
+		path.endsWith(".jpg") ||
+		path.endsWith(".jpeg") ||
+		path.endsWith(".gif")
+	);
+}
+
+export function getFileContents(
+	basePath: string,
+	path: string
+): string | undefined {
+	const contents = readFileSync(`${basePath}/${path}`);
+
+	try {
+		return Buffer.from(contents).toString("utf8");
+	} catch (e) {
+		console.log(e);
+	}
 }

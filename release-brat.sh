f0dd8bad6769d7b2a80f76933aec5b1bdf372edd
Muness Castle <931+muness@users.noreply.github.com>
2024-02-08 08:37:32 -0500

    fix brat release script

--- release-brat.sh
+++ release-brat.sh
@@ -35,15 +35,10 @@ then
   jq ".version |= \"${NEW_VERSION}\"" package.json > "$TEMP_FILE" || exit 1
   mv "$TEMP_FILE" package.json
 
-  echo "Updating manifest.json"
+  echo "Updating manifest-beta.json"
   TEMP_FILE=$(mktemp)
   jq ".version |= \"${NEW_VERSION}\" | .minAppVersion |= \"${MINIMUM_OBSIDIAN_VERSION}\"" manifest-beta.json > "$TEMP_FILE" || exit 1
-  mv "$TEMP_FILE" manifest.json
-
-  echo "Updating versions.json"
-  TEMP_FILE=$(mktemp)
-  jq ". += {\"${NEW_VERSION}\": \"${MINIMUM_OBSIDIAN_VERSION}\"}" versions.json > "$TEMP_FILE" || exit 1
-  mv "$TEMP_FILE" versions.json
+  mv "$TEMP_FILE" manifest-beta.json
 
   read -p "Create git commit, tag, and push? [y/N] " -n 1 -r
   echo

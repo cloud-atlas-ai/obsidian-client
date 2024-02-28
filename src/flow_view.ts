e5c5517b137a66f108ec7e64a4bf3f6deb30157d
Drazen <drazen@urch.eu>
2024-02-28 09:37:17 +0000

    Spruce it up a bit

--- src/flow_view.ts
+++ src/flow_view.ts
@@ -1,4 +1,4 @@
-import { ItemView, WorkspaceLeaf } from "obsidian";
+import { ItemView, WorkspaceLeaf, setIcon } from "obsidian";
 import CloudAtlasPlugin from "./main";
 
 export const CA_VIEW_TYPE = "flow-view";
@@ -37,14 +37,18 @@ export class FlowView extends ItemView {
 
 		const ul = container.createEl("ul");
 		// Create commands for each flow
-		cloudAtlasFlows.forEach((flowFile) => {
+		cloudAtlasFlows.sort().forEach((flowFile) => {
 			const flow = flowFile.path.split("/")[1].split(".flow.md")[0];
 			const table = ul.createEl("table");
+			table.addClass("cloud-atlas-flow-table");
 			const tr = table.createEl("tr");
 			const td1 = tr.createEl("td");
+			td1.addClass("cloud-atlas-flow-td-half");
 			td1.createEl("span", { text: flow });
 			const td2 = tr.createEl("td");
 			const runBtn = td2.createEl("button", { text: "Run" });
+			setIcon(runBtn, "play");
+			runBtn.addClass("cloud-atlas-flow-btn-primary");
 			runBtn.addEventListener("click", async () => {
 				// console.debug(`Running flow ${flow}`);
 				await this.plugin.runFlow(null, flow);
